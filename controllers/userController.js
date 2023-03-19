9e29310b19163a2d0550d95d2ffc3460378b6fed
FeddericoWayne <jackiew1120@hotmail.com>
2023-03-19 19:57:14 -0400

    completed addFriend POST request

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,9 +15,10 @@ module.exports = {
         User.find({})
         .then((results)=>{
             if (results) {
-                res.status(200).json(results)
+                res.status(200).json(results);
             } else {
-                res.status(404).json({ message: 'No data found!' })
+                res.status(404).json({ message: 'No data found!' });
+                return;
             }
         })
         .catch(err => res.status(400).json(err));
@@ -32,9 +33,10 @@ module.exports = {
         // TODO: add function to populate user's thoughts and friends data
         .then((result)=> {
             if (result) {
-                res.status(200).json(result)
+                res.status(200).json(result);
             } else {
-                res.status(404).json({ message:'User not found!' })
+                res.status(404).json({ message:'User not found!' });
+                return;
             }})
         .catch((err)=> res.status(400).json(err));
     },
@@ -45,7 +47,7 @@ module.exports = {
             updateBody ,
             { new: true })
         .then((result)=> {
-            res.status(200).json(result)
+            res.status(200).json(result);
         })
         .catch(err => res.status(400).json(err)); 
     },
@@ -56,13 +58,68 @@ module.exports = {
         User.findOneAndDelete({ _id:req.params.userId })
         .then((result)=>{
             if (result) {
-                res.status(200).json(result)
+                res.status(200).json(result);
             } else {
-                res.status(404).json({ message: 'User not found!' })
+                res.status(404).json({ message: 'User not found!' });
+                return;
             }
         })
         .catch(err => res.status(400).json(err));
-    }
+    },
+    // add a friend to a user
+    addFriend(req,res) {
+        // assigns POST request params to variables
+        const userId = req.params.userId;
+        const friendId = req.params.friendId;
+
+        if (userId === friendId) {
+            res.status(400).json({ message:'Cannot add self as friend!'});
+            return;
+        };
+
+        User.findOne({ _id:userId })
+        .then((data)=>{
+
+            // if user is not found
+            if (!data) {
+                res.status(404).json({ message:'User not found!'});
+                return;
+            }
+
+            // if friend has already been added before
+            if (data.friends.includes(friendId)) {
+                res.status(409).json({ message:'Cannot add same friend twice!'});
+                return;
+            };
+
+            // if able to locate user
+            if (data) {
+
+                // checks to see if friend's userId is valid
+                User.findOne({ _id:friendId })
+                .then((result)=>{
+                    const currentArray = data.friends;
+                    if (result) {
+                        // pushes new friend's userId to current friend array
+                        currentArray.push(friendId);
+                        // saves updated data to database
+                        data.save();
+                        res.status(200).json(data);
+                    } else if (!result) {
+                        res.status(404).json({ message: 'Friend not found!'});
+                        return;
+                    };
+
+                })
+
+            } else {
+                res.status(404).json({ message:'User not found!'});
+                return;
+            }
+        })
+        .catch(err => res.status(400).json(err));
+
+    },
 };
 
 
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,7 +1,13 @@
 // imports express router
 const router = require('express').Router();
 // imports CRUD functions from userController.js
-const { createUser,getAllUsers,getUser,updateUser,deleteUser } = require('../../controllers/userController');
+const { createUser,
+        getAllUsers,
+        getUser,
+        updateUser,
+        deleteUser,
+        addFriend,
+    } = require('../../controllers/userController');
 
 
 // route: /api/users/
@@ -10,7 +16,8 @@ router.route('/').get(getAllUsers).post(createUser);
 // route: /api/users/:userId
 router.route('/:userId').get(getUser).put(updateUser).delete(deleteUser);
 
-
+// route: /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend);
 
  
 
